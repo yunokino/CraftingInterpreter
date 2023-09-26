0e825ad9da05084d9efabadfdbfad17100088d01
403964037@qq.com <yuki@DESKTOP-T6P08C2.localdomain>
2023-09-26 18:09:20 +0800

    Uodate

--- com/craftinginterpreters/lox/RPN.java
+++ com/craftinginterpreters/lox/RPN.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-class AstPrinter implements Expr.Visitor<String> {
+class RPN implements Expr.Visitor<String> {
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -29,24 +29,36 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     private String parenthesize(String name, Expr... exprs) {
+        // 操作数后置而其他并不
         StringBuilder builder = new StringBuilder();
-        builder.append("(").append(name);
-        for (Expr expr : exprs) {
-            builder.append(" ");
-            builder.append(expr.accept(this));
+        if (name == "group") {
+            builder.append("(").append(name);
+        } else {
+            builder.append("(");
         }
-        builder.append(")");
+        for (Expr expr : exprs) {
+            if(name == "group")
+                builder.append(" ");
+            builder.append(expr.accept(this));
+            if(name != "group")
+                builder.append(" ");
+        }
+        if (name == "group") {
+            builder.append(")");
+        } else {
+            builder.append(name).append(")");
+        }
         return builder.toString();
     }
 
-    // public static void main(String[] args) {
-    //     Expr expression = new Expr.Binary(
-    //             new Expr.Unary(
-    //                     new Token(TokenType.MINUS, "-", null, 1),
-    //                     new Expr.Literal(123)),
-    //             new Token(TokenType.STAR, "*", null, 1),
-    //             new Expr.Grouping(
-    //                     new Expr.Literal(45.67)));
-    //     System.out.println(new AstPrinter().print(expression));
-    // }
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+        System.out.println(new RPN().print(expression));
+    }
 }
